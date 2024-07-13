1b19c1edbf2c1606f994192e9ec0c49ca97990b5
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-07-13 22:32:57 +0900

    fix : css 오류 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,8 @@
-/* @type {import('postcss-load-config').Config} */;
-
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  content: [
-    './app//.{js,ts,jsx,tsx,mdx}',
-    './pages/**/.{js,ts,jsx,tsx,mdx}',
-    './components//*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using src directory:
-    './src//.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
   },
-  plugins: [],
 };
 
 export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,10 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './app//*.{js,ts,jsx,tsx,mdx}',
-    './components//*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   prefix: '',
   theme: {
@@ -16,34 +18,23 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        pretendard: ['Pretendard'],
-      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
-        },
-        informative: {
-          DEFAULT: 'var(--informative)',
-          foreground: 'var(--informative-foreground)',
-        },
-        warning: {
-          DEFAULT: 'var(--warning)',
-          foreground: 'var(--warning-foreground)',
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
@@ -60,105 +51,6 @@ const config = {
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
-        },
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            'h2, h3, h4': {
-              scrollMarginTop: '5rem',
-            },
-            p: {
-              marginTop: '2rem',
-              marginBottom: '2rem',
-            },
-            '.callout-contents > p': {
-              margin: 0,
-            },
-
-            code: {
-              counterReset: 'line',
-            },
-
-            // Inline code only
-            ':not(pre) > code': {
-              fontWeight: 'inherit',
-              position: 'relative',
-              bottom: 1,
-              margin: '0 3px',
-              color: '#eb5757',
-              backgroundColor: 'rgba(135,131,120,0.15)',
-              fontFamily:
-                '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
-              borderRadius: 3,
-              padding: '0.2em 0.4em',
-              overflowWrap: 'break-word',
-            },
-
-            'code::before': {
-              content: 'none',
-            },
-            'code::after': {
-              content: 'none',
-            },
-
-            'code[data-line-numbers] > [data-line]::before': {
-              counterIncrement: 'line',
-              content: 'counter(line)',
-
-              /* Other styling */
-              display: 'inline-block',
-              width: '1rem',
-              marginRight: '1.4rem',
-              textAlign: 'right',
-              color: 'lightgrey',
-              fontSize: '0.75rem',
-            },
-
-            'code[data-line-numbers-max-digits="2"] > [data-line]::before': {
-              width: '1rem',
-            },
-
-            'code[data-line-numbers-max-digits="3"] > [data-line]::before': {
-              width: '2rem',
-            },
-
-            pre: {
-              paddingRight: 0,
-              paddingLeft: 0,
-              color: 'var(--shiki-light)',
-              backgroundColor: 'var(--shiki-light-bg)',
-              border: '1px solid #e5e7eb',
-            },
-
-            '.dark pre': {
-              backgroundColor: 'var(--shiki-dark-bg)',
-              color: 'var(--shiki-dark)',
-              border: '1px solid #374151',
-            },
-
-            'pre > code > span': {
-              paddingLeft: '1.1rem',
-              paddingRight: '1rem',
-            },
-
-            'pre code span': {
-              color: 'var(--shiki-light)',
-            },
-            '.dark pre code span': {
-              color: 'var(--shiki-dark)',
-            },
-
-            '[data-highlighted-line]': {
-              backgroundColor: 'rgba(253, 224, 71, 0.2)',
-            },
-
-            u: {
-              textUnderlineOffset: '4px',
-              textDecorationThickness: 1,
-              fontWeight: 600,
-            },
-          },
         },
       },
       borderRadius: {
@@ -182,8 +74,7 @@ const config = {
       },
     },
   },
-  // eslint-disable-next-line global-require
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
