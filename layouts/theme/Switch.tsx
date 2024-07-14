8be3430227fa4a9f7cfe8fa62fce9d7b9d01c306
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-07-14 23:59:42 +0900

    style : 전체 레이아웃 잡기

--- layouts/theme/Switch.tsx
+++ layouts/theme/Switch.tsx
@@ -22,7 +22,6 @@ function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
   }, []);
