485d7f0cdcdd7267eacc472fa3e5afb8b83ea29a
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-07-22 22:20:35 +0900

    feat : mdx parsing 기능 구현

--- components/post_list/PostList.tsx
+++ components/post_list/PostList.tsx
@@ -1,12 +1,20 @@
-import { getAllPostCount, getCategoryDetailList } from '@/lib/post';
+import {
+  getAllPostCount,
+  getCategoryDetailList,
+  getPostList,
+} from '@/lib/post';
 import CategoryList from '@/components/post_list/CategoryList';
+import PostCard from './PostCard';
 
 interface PostListProps {
   category?: string;
 }
 async function PostListPage({ category }: PostListProps) {
+  const postList = await getPostList(category);
   const categoryList = await getCategoryDetailList();
   const allPostCount = await getAllPostCount();
+
+  console.log(postList.map((data) => data));
 
   return (
     <section className="mx-auto mt-12 w-full max-w-[950px] px-4">
@@ -15,7 +23,13 @@ async function PostListPage({ category }: PostListProps) {
         categoryList={categoryList}
         currentCategory={category}
       />
-      <section />
+      <section>
+        <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12">
+          {postList.map((post) => (
+            <PostCard key={post.url + post.date} post={post} />
+          ))}
+        </ul>
+      </section>
     </section>
   );
 }
--- lib/post.ts
+++ lib/post.ts
@@ -1,10 +1,16 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import readingTime from 'reading-time';
+import dayjs from 'dayjs';
+
 import { sync } from 'glob';
 import path from 'path';
+import { CategoryDetail, HeadingItem, Post, PostMatter } from '@/config/type';
 
 const BASE_PATH = '@/posts';
 const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
 
-export const getPostPath = (category?: string) => {
+export const getPostPaths = (category?: string) => {
   const folder = category || '**';
   const postPath: string[] = sync(`${POSTS_PATH}/${folder}/**/*.mdx`);
   return postPath;
@@ -30,5 +36,103 @@ const parsePostAbstract = (postPath: string) => {
 };
 
 const parsePostDetail = (postPath: string) => {
-  // const file = fs
-}
+  const file = fs.readFileSync(postPath, 'utf8');
+  const { data, content } = matter(file);
+  const grayMatter = data as PostMatter;
+  const readingMinutes = Math.ceil(readingTime(content).minutes);
+  const dateString = dayjs(grayMatter.date)
+    .locale('ko')
+    .format('YYYY년 MM월 DD일');
+  return { ...grayMatter, dateString, content, readingMinutes };
+};
+
+const sortPostList = (PostList: Post[]) =>
+  PostList.sort((a, b) => (a.date > b.date ? -1 : 1));
+
+const parsePost = async (postPath: string): Promise<Post> => {
+  const postAbstract = parsePostAbstract(postPath);
+  const postDetail = await parsePostDetail(postPath);
+  return {
+    ...postAbstract,
+    ...postDetail,
+  };
+};
+
+export const getPostList = async (category?: string): Promise<Post[]> => {
+  const postPaths = getPostPaths(category);
+  const postList = await Promise.all(
+    postPaths.map((postPath) => parsePost(postPath)),
+  );
+  return postList;
+};
+
+export const getSortedPostList = async (category?: string) => {
+  const postList = await getPostList(category);
+  return sortPostList(postList);
+};
+
+export const getSitemapPostList = async () => {
+  const postList = await getPostList();
+  const baseUrl = 'https://www.d5br5.dev';
+  const sitemapPostList = postList.map(({ url }) => ({
+    lastModified: new Date(),
+    url: `${baseUrl}${url}`,
+  }));
+  return sitemapPostList;
+};
+
+export const getAllPostCount = async () => (await getPostList()).length;
+
+export const getCategoryList = () => {
+  const cgPaths: string[] = sync(`${POSTS_PATH}/*`);
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  const cgList = cgPaths.map((path) => path.split('/').slice(-1)?.[0]);
+  return cgList;
+};
+
+export const getCategoryDetailList = async () => {
+  const postList = await getPostList();
+  const result: { [key: string]: number } = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (const post of postList) {
+    const category = post.categoryPath;
+    if (result[category]) {
+      result[category] += 1;
+    } else {
+      result[category] = 1;
+    }
+  }
+  const detailList: CategoryDetail[] = Object.entries(result).map(
+    ([category, count]) => ({
+      dirName: category,
+      publicName: getCategoryPublicName(category),
+      count,
+    }),
+  );
+
+  return detailList;
+};
+
+export const getPostDetail = async (category: string, slug: string) => {
+  const filePath = `${POSTS_PATH}/${category}/${slug}/content.mdx`;
+  const detail = await parsePost(filePath);
+  return detail;
+};
+
+export const parseToc = (content: string): HeadingItem[] => {
+  const regex = /^(##|###) (.*$)/gim;
+  const headingList = content.match(regex);
+  return (
+    headingList?.map((heading: string) => ({
+      text: heading.replace('##', '').replace('#', ''),
+      link: `#${heading
+        .replace('# ', '')
+        .replace('#', '')
+        .replace(/[\\[\]:!@#$/%^&*()+=,.]/g, '')
+        .replace(/ /g, '-')
+        .toLowerCase()
+        .replace('?', '')}`,
+      indent: (heading.match(/#/g)?.length || 2) - 2,
+    })) || []
+  );
+};
