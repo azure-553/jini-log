eea66e0ce5c6e8cd2efbb05d5c7857391dda2c5e
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-06 18:11:26 +0900

    feat: category에 따라 글 보여주기

--- app/(routes)/blog/[category]/page.tsx
+++ app/(routes)/blog/[category]/page.tsx
@@ -1,5 +1,12 @@
+import PostListPage from '@/components/post_list/PostList';
 import React from 'react';
 
-export default function CategoryPage() {
-  return <div>CategoryPage</div>;
+type Props = {
+  params: { category: string };
+};
+
+async function CategoryPage({ params }: Props) {
+  return <PostListPage category={params.category} />;
 }
+
+export default CategoryPage;
