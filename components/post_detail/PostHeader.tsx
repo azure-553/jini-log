db3a8807030dbd07389579b9a8874cd2ac23c35c
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-06 20:02:41 +0900

    style: post header 레이아웃 추가

--- components/post_detail/PostHeader.tsx
+++ components/post_detail/PostHeader.tsx
@@ -1,4 +1,6 @@
 import { Post } from '@/config/type';
+import { CalendarDays, Clock3 } from 'lucide-react';
+import Link from 'next/link';
 
 interface Props {
   post: Post;
@@ -6,8 +8,27 @@ interface Props {
 
 function PostHeader({ post }: Props) {
   return (
-    <header>
-      <h1>{post.title}</h1>
+    <header className="mt-14 text-center">
+      <h1 className="mb-5 text-[2rem] font-bold">{post.title}</h1>
+      <div className="mb-3 text-base">
+        <Link
+          href={`/blog/${post.categoryPath}`}
+          className="font-semibold text-pink-600 no-underline underline-offset-4 hover:underline"
+        >
+          {post.categoryPublicName}
+        </Link>
+      </div>
+      <div className="flex justify-center gap-3 text-sm text-gray-500 dark:text-gray-400">
+        <div className="flex items-center gap-1">
+          <CalendarDays className="w-3.5" />
+          <span>{post.dateString}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <Clock3 className="w-3.5" />
+          <span>{post.readingMinutes}분</span>
+        </div>
+      </div>
+      <hr className="mt-5" />
     </header>
   );
 }
